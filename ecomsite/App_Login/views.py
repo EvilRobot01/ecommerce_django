3d85d59c894c49ab94e5a15456bcca5f41a62271
HaroldDRF <95robots@gmail.com>
2021-01-01 18:27:40 -0700

    Products View completed

--- ecomsite/App_Login/views.py
+++ ecomsite/App_Login/views.py
@@ -34,15 +34,15 @@ def login_user(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponse('Logged In')
+                return HttpResponseRedirect(reverse('App_Shop:home'))
     return render(request, 'App_Login/login.html', context={'form':form})
 
 
 @login_required
 def logout_user(request):
     logout(request)
-    messages.success(request, 'You are logged out')
-    return HttpResponse('Logged Out')
+    messages.warning(request, 'You are logged out')
+    return HttpResponseRedirect(reverse('App_Shop:home'))
 
 
 @login_required
--- ecomsite/App_Shop/urls.py
+++ ecomsite/App_Shop/urls.py
@@ -3,5 +3,5 @@ from App_Shop import views
 
 app_name = 'App_Shop'
 urlpatterns = [
-
+    path('', views.Home.as_view(), name='home'),
 ]
